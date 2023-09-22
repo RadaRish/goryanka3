cfc8a632e4662a86f8f53f4ea8f2a8e5637193da
RadaRish <107124341+RadaRish@users.noreply.github.com>
2023-09-22 22:31:06 +0300

    downloaded goryanka 3

---
+++ data.js
@@ -0,0 +1,589 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0---",
+      "name": "Ул. Г. Гасанова",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -3.0191117209198417,
+        "pitch": -0.07119816372635768,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1066403817473898,
+          "pitch": 0.1097962455221797,
+          "rotation": 0,
+          "target": "1---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1---",
+      "name": "Дорога к входу",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.9346915292767832,
+        "pitch": -0.029442614397229505,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0867726622282916,
+          "pitch": 0.0030603337561778687,
+          "rotation": 0,
+          "target": "2---"
+        },
+        {
+          "yaw": -1.5347419003644909,
+          "pitch": 0.032035381424982745,
+          "rotation": 0,
+          "target": "0---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2---",
+      "name": "Вход в здание",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.02327105669326457,
+        "pitch": 0.022798754408437816,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.059079512821098135,
+          "pitch": 0.20932015542351934,
+          "rotation": 0,
+          "target": "3--"
+        },
+        {
+          "yaw": -2.9633848786295225,
+          "pitch": 0.18317769170335474,
+          "rotation": 0,
+          "target": "1---"
+        },
+        {
+          "yaw": 2.0910363096526634,
+          "pitch": 0.046096726661687626,
+          "rotation": 0,
+          "target": "0---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--",
+      "name": "Первый этаж",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -3.095766285803329,
+        "pitch": 0.05066389868539822,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5741542044220749,
+          "pitch": 0.17707589213088504,
+          "rotation": 0,
+          "target": "2---"
+        },
+        {
+          "yaw": -3.0221581083895632,
+          "pitch": -0.06866203800607806,
+          "rotation": 0,
+          "target": "4---2--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4---2--",
+      "name": "Лестница на 2-й этаж",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 3.1218053578896825,
+        "pitch": -0.10406489009194253,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0780619107589384,
+          "pitch": -0.04610831463513243,
+          "rotation": 0,
+          "target": "5--"
+        },
+        {
+          "yaw": -1.8002044547850033,
+          "pitch": 0.9556726967805638,
+          "rotation": 0,
+          "target": "3--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5--",
+      "name": "Второй этаж",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.8439208936733191,
+        "pitch": -0.008709163621931637,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.831086766641734,
+          "pitch": -0.047176251529336355,
+          "rotation": 0,
+          "target": "6---3--"
+        },
+        {
+          "yaw": -1.46451810556335,
+          "pitch": 0.7413177856224404,
+          "rotation": 0,
+          "target": "4---2--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6---3--",
+      "name": "Лестница на 3-й этаж",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.6084518831793346,
+        "pitch": 0.20533298231389807,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0086040442745148,
+          "pitch": -0.23442528581424504,
+          "rotation": 0,
+          "target": "7------"
+        },
+        {
+          "yaw": -1.7653962457368824,
+          "pitch": 0.9390788655280691,
+          "rotation": 0,
+          "target": "5--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7------",
+      "name": "Третий этаж и вход в студию",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -3.089518184230858,
+        "pitch": 0.07102420129375808,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.35737488119583105,
+          "pitch": 0.7453079092048025,
+          "rotation": 0,
+          "target": "6---3--"
+        },
+        {
+          "yaw": -2.9055570643734114,
+          "pitch": 0.5371318198440882,
+          "rotation": 0,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-",
+      "name": "Ресепшен",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.8472145097466637,
+        "pitch": -0.007560732822877725,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0039980724232755,
+          "pitch": 0.4407600523704396,
+          "rotation": 12.566370614359176,
+          "target": "9--"
+        },
+        {
+          "yaw": 1.5973875385031597,
+          "pitch": 0.2586560074226121,
+          "rotation": 0,
+          "target": "7------"
+        },
+        {
+          "yaw": -1.8628301047545968,
+          "pitch": 0.22798083850951123,
+          "rotation": 0,
+          "target": "10-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9--",
+      "name": "Зона отдыха",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.4752659870800215,
+        "pitch": 0.3392789679078625,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2124782533514775,
+          "pitch": 0.5458712004015496,
+          "rotation": 6.283185307179586,
+          "target": "8-"
+        },
+        {
+          "yaw": -1.0525220092529182,
+          "pitch": 0.30400391127270154,
+          "rotation": 0.7853981633974483,
+          "target": "10-"
+        },
+        {
+          "yaw": -2.6382014933906284,
+          "pitch": 0.1595608176303216,
+          "rotation": 10.995574287564278,
+          "target": "7------"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-",
+      "name": "Коридор",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.8347795042864874,
+        "pitch": 0.17105157879490918,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4354193341818338,
+          "pitch": 0.3226607633836771,
+          "rotation": 0,
+          "target": "8-"
+        },
+        {
+          "yaw": 2.575966306854448,
+          "pitch": 0.3179602442335412,
+          "rotation": 0,
+          "target": "11--"
+        },
+        {
+          "yaw": -0.05415889199404411,
+          "pitch": 0.17937092586658032,
+          "rotation": 0,
+          "target": "12--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11--",
+      "name": "Массажный кабинет",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.4836283796224059,
+        "pitch": 0.2153215694129802,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.37706684608872365,
+          "pitch": 0.16965936689692995,
+          "rotation": 0,
+          "target": "10-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12--",
+      "name": "Аппаратный кабинет",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.060332668056613414,
+        "pitch": 0.15972921813326835,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8826681131440335,
+          "pitch": 0.26907608599052324,
+          "rotation": 0,
+          "target": "10-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Горянка",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
